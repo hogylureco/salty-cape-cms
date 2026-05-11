18a15c7d9bcdc50aba8660ebb42f71410d23c7af
hogylureco <hogylureco@gmail.com>
2026-05-11 12:39:18 -0400

    Update index.ts with new Schema Types

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -4,6 +4,15 @@ import {regionType} from './regionType'
 import {structureType} from './structureType'
 import {targetSpeciesType} from './targetSpeciesType'
 import {baitfishType} from './baitfishType'
+import {parentLureType.ts} from './parentLureType.ts'
+import {lureCatalogType.ts} from './lureCatalogType.ts'
+import {lureGearCategoryType.ts} from './lureGearCategoryType.ts'
+import {methodType.ts} from './methodType.ts'
+import {postType.ts} from './postType.ts'
+import {platformType.ts} from './platformType.ts'
+import {pseasonType.ts} from './seasonType.ts'
+import {approachType.ts} from './approachType.ts'
+import {techniqueRetrieveType.ts} from './techniqueRetrieveType.ts'
 
 export const schemaTypes = [
   spotType,
@@ -12,4 +21,13 @@ export const schemaTypes = [
   structureType,
   targetSpeciesType,
   baitfishType,
+  parentLureType,
+  lureCatalogType,
+  lureGearCategoryType,
+  methodType,
+  postType,
+  platformType,
+  seasonType,
+  approachType,
+  techniqueRetrieveType
 ]
