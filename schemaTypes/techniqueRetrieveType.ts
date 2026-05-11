bf4d5b81ef69f56b433f6ffced8e2ab07a8daab0
hogylureco <hogylureco@gmail.com>
2026-05-11 12:36:15 -0400

    Create techniqueRetrieveType.ts

---
+++ schemaTypes/techniqueRetrieveType.ts
@@ -0,0 +1,31 @@
+// schemaTypes/techniqueRetrieveType.ts
+import {defineField, defineType} from 'sanity'
+
+export const regionType = defineType({
+  name: 'technique-retrieve',
+  title: 'Technique Retrieve',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {source: 'name', maxLength: 96},
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+  ],
+  preview: {
+    select: {title: 'name'},
+  },
+})
