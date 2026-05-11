f232da58cd5e25b1907e93cf041f0362d3986ce4
hogylureco <hogylureco@gmail.com>
2026-05-11 12:53:26 -0400

    Update platformType.ts

--- schemaTypes/platformType.ts
+++ schemaTypes/platformType.ts
@@ -7,6 +7,12 @@ export const platformType = defineType({
   type: 'document',
   fields: [
     defineField({
+      name: 'id',
+      title: 'ID',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
       name: 'name',
       title: 'Name',
       type: 'string',
