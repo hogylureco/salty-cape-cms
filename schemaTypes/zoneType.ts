e24252659fa3367b951c3e0db0d9a9ea958b5612
hogylureco <hogylureco@gmail.com>
2026-05-11 12:54:27 -0400

    Update zoneType.ts

--- schemaTypes/zoneType.ts
+++ schemaTypes/zoneType.ts
@@ -7,6 +7,12 @@ export const zoneType = defineType({
   type: 'document',
   fields: [
     defineField({
+      name: 'id',
+      title: 'ID',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
       name: 'name',
       title: 'Name',
       type: 'string',
