c8dee7b34a4f75409954efe95791236b30570670
hogylureco <hogylureco@gmail.com>
2026-05-11 12:34:33 -0400

    Create postType.ts

---
+++ schemaTypes/postType.ts
@@ -0,0 +1,31 @@
+// schemaTypes/postType.ts
+import {defineField, defineType} from 'sanity'
+
+export const regionType = defineType({
+  name: 'post-type',
+  title: 'Post Type',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {source: 'name', maxLength: 96},
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+  ],
+  preview: {
+    select: {title: 'name'},
+  },
+})
