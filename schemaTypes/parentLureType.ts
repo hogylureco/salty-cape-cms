10d5753198bf3a83e10f66fa0493ae3b9618a796
hogylureco <hogylureco@gmail.com>
2026-05-11 12:32:40 -0400

    Create parentLureType.ts

---
+++ schemaTypes/parentLureType.ts
@@ -0,0 +1,31 @@
+// schemaTypes/parentLureType.ts
+import {defineField, defineType} from 'sanity'
+
+export const regionType = defineType({
+  name: 'parent-lure',
+  title: 'Parent Lure',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {source: 'name', maxLength: 96},
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+  ],
+  preview: {
+    select: {title: 'name'},
+  },
+})
