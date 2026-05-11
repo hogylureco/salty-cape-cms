ffe380b1417a3915f5879603b9a4e5889eba1195
hogylureco <hogylureco@gmail.com>
2026-05-11 12:34:08 -0400

    Create methodType.ts

---
+++ schemaTypes/methodType.ts
@@ -0,0 +1,31 @@
+// schemaTypes/methodType.ts
+import {defineField, defineType} from 'sanity'
+
+export const regionType = defineType({
+  name: 'method',
+  title: 'Method',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {source: 'name', maxLength: 96},
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+  ],
+  preview: {
+    select: {title: 'name'},
+  },
+})
