e256357ee33b557d12fa896c3c71079dcfbcd566
hogylureco <hogylureco@gmail.com>
2026-05-11 12:33:42 -0400

    Create lureGearCategoryType.ts

---
+++ schemaTypes/lureGearCategoryType.ts
@@ -0,0 +1,31 @@
+// schemaTypes/lureGearCategoryType.ts
+import {defineField, defineType} from 'sanity'
+
+export const regionType = defineType({
+  name: 'lure-gear-category',
+  title: 'Lure Gear Category',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {source: 'name', maxLength: 96},
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+  ],
+  preview: {
+    select: {title: 'name'},
+  },
+})
