c4798158e68d31ea7997c68b816c6b89ea0e4ac9
hogylureco <hogylureco@gmail.com>
2026-05-19 10:55:42 -0400

    Update techniqueRetrieveType.ts - name

--- schemaTypes/techniqueRetrieveType.ts
+++ schemaTypes/techniqueRetrieveType.ts
@@ -2,7 +2,7 @@
 import {defineField, defineType} from 'sanity'
 
 export const techniqueRetrieveType = defineType({
-  name: 'technique-retrieve',
+  name: 'techniqueRetrieve',
   title: 'Technique Retrieve',
   type: 'document',
   fields: [
