fac9386bd55a4e9b8e0de6979cc89c403c9140e8
hogylureco <hogylureco@gmail.com>
2026-06-09 12:04:53 -0400

    Update sanity.config.ts - add bulkfieldimporttool

--- sanity.config.ts
+++ sanity.config.ts
@@ -26,17 +26,10 @@ export default defineConfig({
       title: 'Bulk Import',
       component: BulkImportTool,
     },
+    bulkJsonFieldImportTool,
   ],
   
 })
 
 
 
-export default defineConfig({
-  name: 'default',
-  title: 'Salty Cape',
-  projectId: 'y83nf1qy',
-  dataset: 'production',
-  // ...your plugins, schema, etc.
-  tools: (prev) => [...prev, bulkJsonFieldImportTool],
-})
