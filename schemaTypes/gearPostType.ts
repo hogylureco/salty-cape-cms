9beee4264b831ec9d68664306c849321df3d4bbf
hogylureco <hogylureco@gmail.com>
2026-06-19 10:37:33 -0400

    Create gearPostType.ts

---
+++ schemaTypes/gearPostType.ts
@@ -0,0 +1,42 @@
+// schemaTypes/gearPostType.ts
+import {defineField, defineType} from 'sanity'
+
+export const gearPostType = defineType({
+  name: 'gearPost',
+  title: 'Gear Posts',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'id',
+      title: 'ID',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {source: 'name', maxLength: 96},
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'array',
+      of: [
+        {type: 'block'},
+        {type: 'image', options: {hotspot: true}},
+        {type: 'richTableBlock'},
+      ],
+    }),
+  ],
+  preview: {
+    select: {title: 'name', subtitle: 'region.name'},
+  },
+})
