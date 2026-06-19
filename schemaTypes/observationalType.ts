cf78f545e8250dca7a2ef20eca108b92aa845a26
hogylureco <hogylureco@gmail.com>
2026-06-19 09:18:57 -0400

    Create observationalType.ts

---
+++ schemaTypes/observationalType.ts
@@ -0,0 +1,42 @@
+// schemaTypes/observationalType.ts
+import {defineField, defineType} from 'sanity'
+
+export const observationalType = defineType({
+  name: 'observational',
+  title: 'Observational Factors',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'id',
+      title: 'ID',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {source: 'name', maxLength: 96},
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'array',
+      of: [
+        {type: 'block'},
+        {type: 'image', options: {hotspot: true}},
+        {type: 'richTableBlock'},
+      ],
+    }),
+  ],
+  preview: {
+    select: {title: 'name', subtitle: 'region.name'},
+  },
+})
