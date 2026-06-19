7bdfaf42415d006e84e14dcc1c608a5877cbe0ae
hogylureco <hogylureco@gmail.com>
2026-06-19 09:17:32 -0400

    Create historicalType.ts

---
+++ schemaTypes/historicalType.ts
@@ -0,0 +1,42 @@
+// schemaTypes/historicalType.ts
+import {defineField, defineType} from 'sanity'
+
+export const historicalType = defineType({
+  name: 'historical',
+  title: 'Historical Factors',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'id',
+      title: 'ID',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {source: 'name', maxLength: 96},
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'array',
+      of: [
+        {type: 'block'},
+        {type: 'image', options: {hotspot: true}},
+        {type: 'richTableBlock'},
+      ],
+    }),
+  ],
+  preview: {
+    select: {title: 'name', subtitle: 'region.name'},
+  },
+})
