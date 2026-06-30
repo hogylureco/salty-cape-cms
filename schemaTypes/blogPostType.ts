d83012c816d626d29fbbd9b842ee916596c315b5
hogylureco <hogylureco@gmail.com>
2026-06-30 13:09:48 -0400

    Update blogPostType.ts - add reference fields

--- schemaTypes/blogPostType.ts
+++ schemaTypes/blogPostType.ts
@@ -25,6 +25,93 @@ export const blogPostType = defineType({
       options: {source: 'name', maxLength: 96},
       validation: (Rule) => Rule.required(),
     }),
+
+    defineField({
+      name: 'method',
+      title: 'Method',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'method'}]}],
+    }),
+
+    defineField({
+      name: 'region',
+      title: 'Region',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'region'}]}],
+    }),
+
+    defineField({
+      name: 'spot',
+      title: 'Spot',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'spot'}]}],
+    }),
+
+
+   defineField({
+      name: 'structure',
+      title: 'Structure',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'structure'}]}],
+    }),
+
+
+       defineField({
+      name: 'approach',
+      title: 'Approach',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'approach'}]}],
+    }),
+
+
+           defineField({
+      name: 'season',
+      title: 'Season',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'season'}]}],
+    }),
+
+
+           defineField({
+      name: 'techniqueRetrieve',
+      title: 'Technique',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'techniqueRetrieve'}]}],
+    }),
+
+
+    defineField({
+      name: 'lureGearCategory',
+      title: 'Lure Gear Category',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'lureGearCategory'}]}],
+    }),
+
+
+    defineField({
+      name: 'parentLure',
+      title: 'Parent Lure',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'parentLure'}]}],
+    }),
+
+
+   defineField({
+      name: 'targetSpecies',
+      title: 'Target Species',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'targetSpecies'}]}],
+    }),
+
+
+       defineField({
+      name: 'baitfish',
+      title: 'Baitfish',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'baitfish'}]}],
+    }),
+
+    
     defineField({
       name: 'description',
       title: 'Description',
